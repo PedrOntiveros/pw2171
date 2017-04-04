14e271eb2d931ee52c52dc1dd9b504298d3c0e49
PedrOntiveros <pedrontiveros94@gmail.com>
2017-04-04 15:02:17 -0600

    Calculadora con JS

---
+++ js/calculadora.js
@@ -0,0 +1,39 @@
+//Varaiable global
+var operador = "";
+
+function igual () {
+	var valor1 = document.calculadora.operando1.value;
+	var valor2 = document.calculadora.operando2.value;
+	document.calculadora.resultado.value = eval(valor1+operador+valor2);
+}
+
+function borrar () {
+	operador = "";
+	document.calculadora.operando1.value = 0;
+	document.calculadora.operando2.value = 0;
+	document.calculadora.resultado.value = 0;
+}
+
+function operadores (ope){
+	operador = ope;
+}
+
+function numeros (num) {
+	if( operador == ""){ //Escribir en el operando1
+		var valorInicial = document.calculadora.operando1.value;
+		if(valorInicial == "0"){
+			//Elimino el cero
+			document.calculadora.operando1.value = "";
+		}
+		//Concatena los valores de num con los del operando1
+		document.calculadora.operando1.value = document.calculadora.operando1.value + num;
+	}else{ //Escribo en el operando 2
+		var valorInicial = document.calculadora.operando2.value;
+		if(valorInicial == "0"){
+			//Elimino el cero
+			document.calculadora.operando2.value = "";
+		}
+		//Concatena los valores de num con los del operando2
+		document.calculadora.operando2.value = document.calculadora.operando2.value + num;
+	}
+}
