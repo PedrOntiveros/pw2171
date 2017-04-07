41256acbea0c8319d74f8757847a0b24471bcae3
PedrOntiveros <pedrontiveros94@gmail.com>
2017-04-07 15:00:30 -0600

    Aplicacion de escritorio, ya porfin vacaciones

--- calculadora/index.js
+++ calculadora/index.js
@@ -2,34 +2,53 @@
 const rq = require('electron-require');
 const main = rq.remote('./main.js');
 var botonesNumero = ["btn00","btn01","btn02","btn03","btn04","btn05","btn06","btn07","btn08","btn09"];
-var botonesOperador = ["btnSumar","btnRestar","btnMultiplicar","btnDividir"]
+var botonesOperador = ["btnSumar","btnRestar","btnMultiplicar","btnDividir"];
+var operador="";
 
 function numeros(){
-	alert.(this.id);
-	alert.(this.value);
+	var num = String(this.value);
+	if(operador == ""){ //validando el operando 1
+		var va = document.getElementById("operando1").value
+		if(va == "0"){
+			document.getElementById("operando1").value = "";
+		}
+		document.getElementById("operando1").value += num;
+	}else{
+		 //validando el operando 2
+		var va = document.getElementById("operando2").value
+		if(va == "0"){
+			document.getElementById("operando2").value = "";
+		}
+		document.getElementById("operando2").value += num;
+	}
 }
 
 function operadores(){
-
+	operador = String(this.value); //+,-,*,/
 }
 
 function igual(){
-
+	var valor1 = document.getElementById("operando1").value;
+	var valor2 = document.getElementById("operando2").value;
+	document.getElementById("resultado").value = eval(valor1+operador+valor2);
 }
 
 function borrar(){
-
+ 	document.getElementById("operando1").value = "0";
+	document.getElementById("operando2").value = "0";
+	document.getElementById("resultado").value = "0";
+	operador = "";
 }
 
 //Asignación de eventos a los botones de número
-for(var i=0; i<botonesNumero.lenght; i++){
-	document.getElementByID((botonesNumero[i]).addEventListener('click',numeros);
+for(var i=0; i<botonesNumero.length; i++){
+	document.getElementById(botonesNumero[i]).addEventListener('click',numeros);
 }
 //Asignación de eventos a los botones de operador
-for(var i=0; i<botonesOperador.lenght; i++){
-	document.getElementByID((botonesOperador[i]).addEventListener('click',operadores);
+for(var i=0; i<botonesOperador.length; i++){
+	document.getElementById(botonesOperador[i]).addEventListener('click',operadores);
 }
 //Evento Click del boton Igual
-document.getElementByID(("btnIgual").addEventListener('click',igual);
+document.getElementById("btnIgual").addEventListener('click',igual);
 //Evento Click del boton CE
-document.getElementByID(("btnCE").addEventListener('click',borrar);
+document.getElementById("btnCE").addEventListener('click',borrar);
